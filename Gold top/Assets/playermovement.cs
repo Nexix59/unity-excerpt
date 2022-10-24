b49fdacb43f3546bb797585ea4cedd00bd5c5c8d
Nexix <pandadank5@gmail.com>
2022-10-23 18:04:03 -0700

    rggr
    gewegewg

---
+++ Gold top/Assets/playermovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        public float moveSeed() 
+        {
+        
+        }
+    }
+    void FixedUpdate()
+    {
+        
+    }
+
+    void ProcessInput()
+    {
+
+    }
+
+    void O
+}
